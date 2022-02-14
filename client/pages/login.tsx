9bcb6962a4f902be94dbf1952b5cbdc7267e355a
Daniel <danpark105@gmail.com>
2022-02-13 21:52:43 -0500

    unnecessary amount of time figuring out useref with ts

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,11 +1,19 @@
-import React, { useRef } from "react";
+import axios from "axios";
+import React, { useContext, useRef } from "react";
+import { AuthContext } from "../context/AuthProvider";
 
 const Login: React.FC<{}> = ({}) => {
-  const username = useRef();
-  const password = useRef();
+  const username = useRef<HTMLInputElement | null>(null);
+  const password = useRef<HTMLInputElement | null>(null);
+  const value = useContext(AuthContext);
 
-  const handleGuh = (e: any) => {
+  const handleGuh = async (e: any) => {
     e.preventDefault();
+    const res = await axios.post("/api/login", {
+      "username": username.current?.value,
+      "password": password.current?.value,
+    });
+    console.log(res);
   };
   return (
     <div className="h-screen flex bg-gray-100">
@@ -18,18 +26,18 @@ const Login: React.FC<{}> = ({}) => {
             <label htmlFor="username">Username</label>
             <input
               type="username"
-              id="username"
               placeholder="Username"
               className={`w-full p-2 border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+              ref={username}
             />
           </div>
           <div>
             <label htmlFor="password">Password</label>
             <input
               type="password"
-              id="password"
               placeholder="Password"
               className={`w-full p-2 border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+              ref={password}
             />
           </div>
           <div className="flex justify-center items-center mt-6">
