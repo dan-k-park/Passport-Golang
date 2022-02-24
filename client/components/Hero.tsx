beadc8c95c4d989f19eddae775b4c2dccf475107
Daniel <danpark105@gmail.com>
2022-02-24 05:46:06 -0500

    slight mods to hero section

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -3,13 +3,15 @@ import React from "react";
 export const Hero: React.FC = ({}) => {
   return (
     <div className="h-[650px] bg-hero bg-center bg-cover flex justify-center items-end">
-      <div className="mb-[70px] flex flex-col">
-        <h1 className="text-white text-3xl mb-[20px]">Where To?</h1>
+      <div className="mb-[70px] lg:mb-[120px] flex flex-col items-center">
+        <h1 className="text-white text-3xl md:text-4xl lg:text-5xl mb-[20px]">
+          Where to Next?
+        </h1>
         <button
           type="button"
-          className="text-rose-400 bg-white hover:bg-slate-200 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          className="text-rose-400 w-1/2 bg-white hover:bg-slate-200 font-semibold rounded-full text-xs md:text-sm lg:text-lg  px-3 py-2.5 text-center mr-2 mb-2"
         >
-          Guh
+          My Passport
         </button>
       </div>
     </div>
--- client/components/PopularSection.tsx
+++ client/components/PopularSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const PopularSection: React.FC = ({}) => {
   return (
-    <div className="h-[400px] max-w-full bg-purple-500 relative">
+    <div className="h-[800px] max-w-full bg-purple-500 relative">
       <div className="absolute top-1 p-10 w-full">
         <p className="text-lg">Most Favorited Countries</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 py-5">
