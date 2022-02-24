a88209fc17a3e7271379493c5a305ac7ae809e46
Daniel <danpark105@gmail.com>
2022-02-24 01:14:23 -0500

    guh

--- client/components/PopularSection.tsx
+++ client/components/PopularSection.tsx
@@ -2,68 +2,60 @@ import React from "react";
 
 export const PopularSection: React.FC = ({}) => {
   return (
-    <div className="h-[400px] max-w-full bg-purple-500 flex flex-col justify-center relative">
-      <div className="absolute top-3.5">
+    <div className="h-[400px] max-w-full bg-purple-500 relative">
+      <div className="absolute top-1 p-10 w-full">
         <p className="text-lg">Most Favorited Countries</p>
-        <div className="grid grid-cols-4 gap-5 max-w-full">
-          <div className="w-[10] bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 py-5">
+          <div className="max-w-[15rem] bg-rose-400 mb-2 rounded-lg shadow-lg">
             <img
-              className="rounded-t-lg"
+              className="w-full h-1/2 rounded-t-lg"
               src="/countryCardPlaceholder.jpg"
-              alt="Picture of a country with a lot of favorites"
+              alt="product"
             />
-            <div className="p-5">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <div className="px-6 py-4">
+              <h4 className="mb-3 text-xl font-semibold tracking-tight text-white">
                 Adobrasig
-              </h5>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                10 Favorites
-              </p>
+              </h4>
+              <p className="leading-normal text-white">10 Favorites</p>
             </div>
           </div>
-          <div className="w-[10] bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <div className="max-w-[15rem] bg-rose-400 mb-2 rounded-lg shadow-lg">
             <img
-              className="rounded-t-lg"
+              className="w-full h-1/2 rounded-t-lg"
               src="/countryCardPlaceholder.jpg"
-              alt="Picture of a country with a lot of favorites"
+              alt="product"
             />
-            <div className="p-5">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <div className="px-6 py-4">
+              <h4 className="mb-3 text-xl font-semibold tracking-tight text-white">
                 Adobrasig
-              </h5>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                10 Favorites
-              </p>
+              </h4>
+              <p className="leading-normal text-white">10 Favorites</p>
             </div>
           </div>
-          <div className="w-[10] bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <div className="max-w-[15rem] bg-rose-400 mb-2 rounded-lg shadow-lg">
             <img
-              className="rounded-t-lg"
+              className="w-full h-1/2 rounded-t-lg"
               src="/countryCardPlaceholder.jpg"
-              alt="Picture of a country with a lot of favorites"
+              alt="product"
             />
-            <div className="p-5">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <div className="px-6 py-4">
+              <h4 className="mb-3 text-xl font-semibold tracking-tight text-white">
                 Adobrasig
-              </h5>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                10 Favorites
-              </p>
+              </h4>
+              <p className="leading-normal text-white">10 Favorites</p>
             </div>
           </div>
-          <div className="w-[10] bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <div className="max-w-[15rem] bg-rose-400 mb-2 rounded-lg shadow-lg">
             <img
-              className="rounded-t-lg"
+              className="w-full h-1/2 rounded-t-lg"
               src="/countryCardPlaceholder.jpg"
-              alt="Picture of a country with a lot of favorites"
+              alt="product"
             />
-            <div className="p-5">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <div className="px-6 py-4">
+              <h4 className="mb-3 text-xl font-semibold tracking-tight text-white">
                 Adobrasig
-              </h5>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                10 Favorites
-              </p>
+              </h4>
+              <p className="leading-normal text-white">10 Favorites</p>
             </div>
           </div>
         </div>
