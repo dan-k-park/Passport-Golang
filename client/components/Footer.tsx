eb2b51b2264522b8892dde765535bb109b90b9d5
Daniel <danpark105@gmail.com>
2022-03-08 23:13:27 -0800

    simple footer, next step redux

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export const Footer: React.FC = ({}) => {
   return (
-    <div className="h-[600px] bg-slate-500">
-      Ah geez a footer I could do like a meter
+    <div className="h-[25px] flex justify-end items-center">
+      <p className="text-xl mr-[15px] mb-[15px]">
+        Passport © {new Date().getFullYear()} All rights reserved.
+      </p>
     </div>
   );
 };
